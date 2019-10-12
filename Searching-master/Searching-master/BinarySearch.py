af344b34bc853497b5956c7525e67755956ca4b7
robo171299 <43902679+robo171299@users.noreply.github.com>
2019-10-12 22:51:14 +0530

    Content

---
+++ Searching-master/Searching-master/BinarySearch.py
@@ -0,0 +1,1 @@
+# Enter code for binary search
---
+++ Searching-master/Searching-master/LinearSearch.py
@@ -0,0 +1,8 @@
+# Enter code for Linear Search
+list =[1,5,7,'ROBO','GAME',10,'OVER']
+n = 'OVER!'
+if search(list,n):
+  print("Found!!")
+else:
+  print("Oops...Not Found!!")
+
